807c12e87d7052ea8f362f900b64b197fdbd95b9
sansan20535 <20san20@daum.net>
2023-12-15 23:37:44 +0900

    [Feat]스프링 빈 조회 - 상ã속관계-3

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -35,6 +37,13 @@ public class ApplicationContextExtendsFindTest {
     void findBeanBySubType(){
         RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
         assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기")
+    void findAllBeanByParentType(){
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+        assertThat(beansOfType.size()).isEqualTo(2);
     }
 
     @Configuration
