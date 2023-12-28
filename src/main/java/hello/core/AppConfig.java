f75ed1304b6b54d535dc49ea1b527e36fcf7c79c
sansan20535 <20san20@daum.net>
2023-12-28 20:17:52 +0900

    [Feat]@Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -11,21 +11,30 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//call AppConfig.memberService
+//call AppConfig.memberRepository
+//call AppConfig.memberRepository
+//call AppConfig.orderService
+//call AppConfig.memberRepository
+
 @Configuration //설정 및 구성 정보
 public class AppConfig {
 
     @Bean //스프링 컨테이너 등록
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
